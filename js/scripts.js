3d5770fe1a70d8adb788c108704b9fee57bc3818
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-06-11 23:45:56 -0400

    Added transition to show the changing of time

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,22 @@ var HOURHAND = document.querySelector("#hour");
 var MINUTEHAND = document.querySelector("#minute");
 var SECONDHAND = document.querySelector("#second");
 
-var date = new Date();
-console.log(date);
-var hr = date.getHours();
-var min = date.getMinutes();
-var sec = date.getSeconds();
-console.log("Hour: " + hr + " Minutes: " + min + " Second: " + sec);
+function runTheClock() {
+	var date = new Date();
+	console.log(date);
+	var hr = date.getHours();
+	var min = date.getMinutes();
+	var sec = date.getSeconds();
+	console.log("Hour: " + hr + " Minutes: " + min + " Second: " + sec);
 
-var hrPosition = (hr*360/12)+(min*(360/60)/12);
-var minPosition = (min*360/60)+(sec*(360/60)/60);
-var secPosition = sec*360/60;
- 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+	var hrPosition = (hr*360/12)+(min*(360/60)/12);
+	var minPosition = (min*360/60)+(sec*(360/60)/60);
+	var secPosition = sec*360/60;
+
+	HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+	MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+	SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+
+var interval = setInterval(runTheClock, 1000);
